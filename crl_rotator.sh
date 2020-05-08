3ee44ec7cbd579fbb131641ccdafc811408668e9
Teymur Gahramanov <36300147+teymurgahramanov@users.noreply.github.com>
2020-05-08 18:41:20 +0400

    Update crl_rotator.sh

--- crl_rotator.sh
+++ crl_rotator.sh
@@ -2,48 +2,66 @@
 
 declare -A SOURCES
 
+OWNER_USER=apache
+OWNER_GROUP=apache
+
 SOURCES=(\
-        ["CA_NAME_1"]="https://ca_url_1.com" \
-        ["CA_NAME_2"]="https://ca_url_2.com" \
+        ["CRL_SOURCE_1_NAME"]="CRL_SOURCE_1_URL" \
 )
 
 for K in "${!SOURCES[@]}";
 do
 
         CRL_URL=${SOURCES[$K]}
-        CRL_PATH="/etc/httpd/SSL/CRL"
+        CRL_PATH="/PATH/TO/CRL/DIRECTORY"
         CRL_FILE="$CRL_PATH/$K.crl"
         CRL_STATUS_FILE="$CRL_PATH/$K.crl.status"
 
         wget -q -O /tmp/$K.crl "$CRL_URL"
-
-        if [ $? -ne 0 ];
+        if [ $? == 0 ];
         then
-                echo $date "wget error" >>  /tmp/crl.log
-                exit 1
-        fi
-
-    	#Check if CRL updated
-    	NEW_CRL_VERSION="$(openssl crl -inform der -in /tmp/$K.crl -crlnumber -noout | cut -d= -f2)"
-
-    	#Check version of currently used CRL
-    	if [ -f $CRL_STATUS_FILE ];
-	then
-                LAST_CRL_VERSION=$(cat $CRL_STATUS_FILE)
+                #Check if CRL in PEM format
+                openssl crl -in /tmp/$K.crl -noout > /dev/null 2>&1
+                if [ $? == 0 ];
+                then
+                        #Compare CRL versions to ensure that downloaded is newer
+                        NEW_CRL_VERSION="$(openssl crl -in /tmp/$K.crl -crlnumber -noout | cut -d= -f2)"
+                        if [ -f $CRL_STATUS_FILE ];
+                        then
+                                LAST_CRL_VERSION=$(cat $CRL_STATUS_FILE)
+                        else
+                        LAST_CRL_VERSION=0
+                        fi
+                        if [ "$NEW_CRL_VERSION" != "$LAST_CRL_VERSION" ];
+                        then
+                        echo $NEW_CRL_VERSION > $CRL_STATUS_FILE
+                        openssl crl -in /tmp/$K.crl > $CRL_FILE
+                        chown $OWNER_USER:$OWNER_GROUP $CRL_FILE
+                        chmod 0640 $CRL_FILE
+                        restorecon $CRL_FILE
+                        fi
+                #In case if CRL in DER format
+                else
+                        NEW_CRL_VERSION="$(openssl crl -inform der -in /tmp/$K.crl -crlnumber -noout | cut -d= -f2)"
+                        if [ -f $CRL_STATUS_FILE ];
+                        then
+                                LAST_CRL_VERSION=$(cat $CRL_STATUS_FILE)
+                        else
+                                LAST_CRL_VERSION=0
+                        fi
+                        if [ "$NEW_CRL_VERSION" != "$LAST_CRL_VERSION" ];
+                        then
+                        echo $NEW_CRL_VERSION > $CRL_STATUS_FILE
+                        #Convert DER to PEM
+                        openssl crl -inform der -in /tmp/$K.crl > $CRL_FILE
+                        chown $OWNER_USER:$OWNER_GROUP $CRL_FILE
+                        chmod 0640 $CRL_FILE
+                        restorecon $CRL_FILE
+                        fi
+                fi
         else
-                LAST_CRL_VERSION=0
+                echo "$(date +"%D %T") Unable to download $K.crl" >> /tmp/crl_rotator.log
         fi
-
-		#If CRL updated, convert this new CRL to PEM and set permissions
-        if [ "$NEW_CRL_VERSION" != "$LAST_CRL_VERSION" ];
-        then
-                echo $NEW_CRL_VERSION > $CRL_STATUS_FILE
-                openssl crl -inform der -in /tmp/$K.crl > $CRL_FILE
-                chown owneruser:ownergroup $CRL_FILE
-                chmod 0640 $CRL_FILE
-                restorecon $CRL_FILE
-        fi
-
 done
 
 rm -f /tmp/*.crl
