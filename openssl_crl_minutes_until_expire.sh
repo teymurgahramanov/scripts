e78eb496dbba83a4e08af749c1e96e7ace3de837
Teymur Gahramanov <36300147+teymurgahramanov@users.noreply.github.com>
2020-08-31 00:02:29 +0400

    Update openssl_crl_minutes_until_expire.sh

--- openssl_crl_minutes_until_expire.sh
+++ openssl_crl_minutes_until_expire.sh
@@ -2,8 +2,17 @@
 
 # $1 Path to CRL file
 
-enddate=$(openssl crl -inform DER -in $1 -nextupdate -noout | cut -d = -f2)
-enddate_s=$(date -d "${enddate}" +%s)
-now_s=$(date -d now +%s)
-enddate_m=$(( (enddate_s - now_s) / 60 ))
-echo "$enddate_m"
+openssl crl -in $1 -noout > /dev/null 2>&1
+if [ $? == 0 ]; then
+        enddate=$(openssl crl -in $1 -nextupdate -noout | cut -d = -f2)
+        enddate_s=$(date -d "${enddate}" +%s)
+        now_s=$(date -d now +%s)
+        enddate_m=$(( (enddate_s - now_s) / 60 ))
+        echo "$enddate_m"
+else
+        enddate=$(openssl crl -inform DER -in $1 -nextupdate -noout | cut -d = -f2)
+        enddate_s=$(date -d "${enddate}" +%s)
+        now_s=$(date -d now +%s)
+        enddate_m=$(( (enddate_s - now_s) / 60 ))
+        echo "$enddate_m"
+fi
