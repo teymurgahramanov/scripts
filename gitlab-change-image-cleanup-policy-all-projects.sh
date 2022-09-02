c5467eaf29ed4cbc6e2929620dba8a377bb454cb
Teymur Gahramanov <36300147+teymurgahramanov@users.noreply.github.com>
2022-09-02 15:03:40 +0400

    Update gitlab-change-image-cleanup-policy-all-projects.sh

--- gitlab-change-image-cleanup-policy-all-projects.sh
+++ gitlab-change-image-cleanup-policy-all-projects.sh
@@ -1,10 +1,9 @@
 # Will enable and update image cleanup policy (Project -> Settings -> CI/CD -> Clean up image tags)
 # https://docs.gitlab.com/ee/user/packages/container_registry/reduce_container_registry_storage.html
-# 
 # Requirements: jq
 
-GITLAB_URL="https://gitlab.example.com" # Change it
-TOKEN="asfdk24ij5123j123j15" # Change it
+GITLAB_URL="https://gitlab.example.com"
+TOKEN="asfdk24ij5123j123j15" # Personal Access Token
 ID=$(curl -s "$GITLAB_URL/api/v4/projects?private_token=$TOKEN&per_page=300" | jq '.[] | select(.namespace.name=="example_filter_by_namespace") | .id')
 
 for i in $ID; do \
